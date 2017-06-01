394c813d6b8dacdf7cbc5d003cb9069bdcad6532
Benjamin T. Seaver <BenjaminTSeaver@gmail.com>
2017-06-01 11:44:23 -0700

    New Settings

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -290,7 +290,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '_Q_09redqM12N92Jtu4c1QTkHrr_AnWkgPaF8OWN_hU';
+$drupal_hash_salt = 'LLA57ycTtMp_L-6JgaLqRujckZp00fjDPaM2rcT84FU';
 
 /**
  * Base URL (optional).
